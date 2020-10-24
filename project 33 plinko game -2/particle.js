35f1f35a8ceed1b52bf545466c4c7f4c4867aa4b
Samaesha <65852261+Samaesha@users.noreply.github.com>
2020-10-24 13:13:32 +0530

    Add files via upload

---
+++ project 33 plinko game -2/particle.js
@@ -0,0 +1,27 @@
+class Particle{
+    constructor(x,y){
+      var b_option={
+          'restitution':0.5,
+          'friction':0.3,
+          'density':0.8,
+      }
+    
+    this.body =  Bodies.circle(x ,y , 15 ,b_option);
+    this.radius=15;
+    this.color = color(random(55,255),random(55,255),random(55,255));
+    World.add(world, this.body);
+     }
+
+    display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    fill(this.color);
+    ellipseMode(CENTER);
+    ellipse(0, 0, this.radius,this.radius);
+    pop();
+    }
+}
---
+++ project 33 plinko game -2/plinko.js
@@ -0,0 +1,26 @@
+class Plinko{
+    constructor(x,y){
+      var option={
+          'restitution':1,
+          'friction':0.3,
+          'density':0.8,
+          isStatic: true
+      }
+    this.body =  Bodies.circle(x ,y , 12 ,option); 
+    this.radius =12;
+   
+    World.add(world, this.body);
+     }
+    display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(CENTER);
+    fill("pink");
+    strokeWeight(4);
+    ellipse(0, 0, this.radius,this.radius);
+    pop();
+    }
+}
